c1934626e46004f69e330011bf5016d1820c2cdf
Johan Kämpe <kampe.johan@gmail.com>
2016-12-05 22:53:02 +0100

    Updated comments

--- binaryToFloat/bintofloat.c
+++ binaryToFloat/bintofloat.c
@@ -1,19 +1,12 @@
-//Konvertera binärt tal till decimalt tal
-//Converts binary to decimal
-
-//Cant handle very long numbers
-//Workaround would be to input binary as char array **FIXED
-
-//compile with 
-//gcc bintoint.c ..\lib\libnum.c -o output.exe
+//Converts fraction binary to float
+//for testing:
+//http://www.exploringbinary.com/binary-converter/
 
 #include "../lib/libnum.h"
 #include "../lib/libtxt.h"
 //#define N 300
 
 int main(){
-	//Det binära talet 1011 ska bli 11 i decimalform.
-	//Binary 1011 is 11 in decimal form
 	char binary[N] = "1011.011";
 	while(1){
 		//User string input to char array binary
