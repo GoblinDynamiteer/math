1dbbb411b96523ae7b470c58bfcfe2eba1d52d26
Johan Kämpe <kampe.johan@gmail.com>
2016-12-05 20:29:55 +0100

    Changed code to use numlib function library

--- binaryToInt/bintoint.c
+++ binaryToInt/bintoint.c
@@ -8,10 +8,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
-
-bool isBinary(long long int a);
-int countDigits(long long int a);
-int convertBinaryToDecimal(long long int b);
+#include "../lib/libnum.h"
 
 int main(){
 	//Det binära talet 1011 ska bli 11 i decimalform.
@@ -31,45 +28,4 @@ int main(){
 		}
 	}
 	return 0;
-}
-
-//Converts binary to decimal
-int convertBinaryToDecimal(long long int b){
-	//Gets number of digits
-	int n = countDigits(b);
-	int decimal = 0;
-	for(int i = 0; i<n; i++){
-		bool lastDigit = b % 10;
-		decimal += lastDigit * pow(2,i);
-		b /= 10;
-	}
-	return decimal;
-}
-
-
-//Function to check if int is in binary form (just consists of 1 and 0)
-//Funktion som kontrollerar om int är i binär form (enbart 1 och 0)
-bool isBinary(long long int a){
-	//check every last digit in integer with modulus 10, if rest is not 1 or 0,
-	//the int is not in binary form.
-	while(a != 0){
-		if(a % 10 > 1){
-			printf("a %% 10 = %d", a % 10);
-			return 0;
-		}
-		a /= 10;
-	}
-	return 1;
-}
-
-//Counts and returns number of digits in int
-//Räknar och returnerar antalet siffror i int
-int countDigits(long long int a){
-	int counter = 0;
-	while(a != 0){
-		counter++;
-		//a is equal to integer division by 10 of itself
-		a /= 10;
-	}
-	return counter;
 }
