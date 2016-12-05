e3d823e5c3f2cbe6b3ff037ebd98040ac77ae91d
Johan Kämpe <kampe.johan@gmail.com>
2016-12-05 21:07:33 +0100

    Added functions to convert binary in char array to int

--- lib/libnum.c
+++ lib/libnum.c
@@ -22,6 +22,7 @@ bool isBinary(long long int a){
 	//the int is not in binary form.
 	while(a != 0){
 		if(a % 10 > 1){
+			//Digit larger than 1 found, number is not in binary form
 			return 0;
 		}
 		a /= 10;
@@ -29,7 +30,36 @@ bool isBinary(long long int a){
 	return 1;
 }
 
+//Function to check if char array is in binary form (just consists of 1 and 0)
+bool isBinaryString(char *a){
+	int n = strlen(a);
+	for(int i =0; i<n; i++){
+		int number = a[i] - '0';
+		if(number > 1){
+			return 0;
+		}
+	}
+	return 1;
+}
+
+//Converts string in binary form to decimal
+//eg 101 = 1*2^0  + 0*2^1 + 1*2^2 = 5
+int convertBinaryStringToDecimal(char *a){
+	//Array starts at zero, therefore -1
+	int n = strlen(a) - 1;
+	int decimal = 0;
+	//For power of
+	int i = 0;
+	while(n >= 0){
+		//Digit in char - '0' is digit in int
+		int lastDigit = a[n--] - '0';
+		decimal += lastDigit * pow(2,i++);
+	}
+	return decimal;
+}
+
 //Converts int in binary form to decimal
+//eg 101 = 1*2^0  + 0*2^1 + 1*2^2 = 5
 int convertBinaryToDecimal(long long int b){
 	//Gets number of digits
 	int n = countDigits(b);
--- lib/libnum.h
+++ lib/libnum.h
@@ -1,6 +1,10 @@
 #include <stdbool.h>
 #include <math.h>
+#include <string.h>
+#include <stdio.h>
 
 bool isBinary(long long int a);
+bool isBinaryString(char *a);
 int countDigits(long long int a);
 int convertBinaryToDecimal(long long int b);
+int convertBinaryStringToDecimal(char *a);
