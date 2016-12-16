6596afc2a549d78c5eb106b9e424b0e3c0f871e9
Johan Kämpe <kampe.johan@gmail.com>
2016-12-16 12:27:11 +0100

    Removed some test code

--- intToBinary/inttobin_usinglib.c
+++ intToBinary/inttobin_usinglib.c
@@ -9,12 +9,6 @@ Test av funktionsbibliotek libconvert
 int main(void){
 	char *binstr;
 	double decfloat = 0.1875;
-/* 	int decimal = 1025;
-	binstr = convert_i_to_binary(decimal);
-	printf("\n%d: %s\n", decimal, binstr);
-	double decfloat = 0.1875;
-	binstr = convert_f_to_binary(decfloat);
-	printf("\n%g: %s\n", decfloat, binstr); */
 	while(1){
 		decfloat = inputDouble("Ange decimaltal: ");
 		binstr = convert_to_binary(decfloat);
